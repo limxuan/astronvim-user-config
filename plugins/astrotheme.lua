1cff3363da63a47468776f3c8a86f414318ade26
reconlx <reconlx@protonmail.com>
2023-07-13 23:16:42 +0800

    astrotheme with wiztermmmm

---
+++ plugins/astrotheme.lua
@@ -0,0 +1,13 @@
+return {
+	"AstroNvim/astrotheme",
+	opts = {
+		palette = "astrodark", -- String of the default palette to use when calling `:colorscheme astrotheme`
+		background = {       -- :h background, palettes to use when using the core vim background colors
+			light = "astrolight",
+			dark = "astrodark",
+		},
+		style = {
+			transparent = true,
+		},
+	},
+}
